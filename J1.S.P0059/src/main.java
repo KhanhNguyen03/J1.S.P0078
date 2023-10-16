e60c5da6d10e9c472d5cc8c0680bd5c91ac6c01e
Khanh <khanhnguyn2528@gmail.com>
2023-10-16 23:00:08 +0700

    commit 1

---
+++ J1.S.P0059/src/main.java
@@ -0,0 +1,8 @@
+
+import controller.FileManager;
+
+public class main {
+    public static void main(String[] args) {
+        new FileManager().run();
+    }
+}
---
+++ J1.S.P0059/src/repository/IFileRes.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package repository;
+
+import java.io.File;
+import java.util.List;
+import model.Person;
+
+public interface IFileRes {
+
+    void readFile(List<Person> personList);
+
+    void writeFile();
+
+}
